d89f89f2a8352dada238b494c98e21e66e93c9ca
VinhTruong2001 <ducvinh.truong2001@gmail.com>
2022-04-05 08:32:37 +0700

    UI: responsive header

--- static/js/header.js
+++ static/js/header.js
@@ -2,3 +2,17 @@ $(".header__search-btn").click(() => {
     $(".header__search-btn").toggleClass("active");
     $(".header__search-bar-wrap").toggleClass("active");
 })
+
+$("#toggleMenu").change(() => {
+    $('.overlay').toggleClass("active");
+    $('.header__navbar').toggleClass("active");
+})
+
+$('.overlay').click(() => {
+    if ($('.header__navbar').hasClass("active")) {
+        console.log("clicked");
+        $("#toggleMenu").prop('checked', false);
+        $('.header__navbar').removeClass("active");
+        $('.overlay').removeClass("active");
+    }
+})
