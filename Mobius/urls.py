ed1dfabb9b1e4932f87be3ba24aafd270367ba15
VinhTruong2001 <ducvinh.truong2001@gmail.com>
2022-04-30 20:10:10 +0700

    feat: home page UI

--- Mobius/urls.py
+++ Mobius/urls.py
@@ -1,21 +1,11 @@
-"""Mobius URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from filebrowser.sites import site
+from .views import HomeView, search
+from django.urls import path, include
+from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/filebrowser/', site.urls),
@@ -23,6 +13,9 @@ urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
 
     path('admin/', admin.site.urls),
-    path('', include('Phone.urls')), # new
+    path('', HomeView.as_view(), name='home_view'),
+    path('search/', search, name='search_results'),
+    # path('', include('Phone.urls')),
     path('', include('User.urls')),
+    # path('', include('Blogs.urls')),
 ]
