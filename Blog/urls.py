09ac4086ff308d4717e039e71daa529b7d3f9fa9
VinhTruong2001 <ducvinh.truong2001@gmail.com>
2022-05-31 21:33:32 +0700

    feat: blog list page

--- Blog/urls.py
+++ Blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import BlogView, ListBlog
 
 urlpatterns = [
-    path('tin-tuc/', ListBlog, name='blog'),
+    path('tin-tuc/', ListBlog, name='listBlogs'),
     path('tin-tuc/<str:blogTitle>/', BlogView.as_view(), name="blog_detail"),
 ]
